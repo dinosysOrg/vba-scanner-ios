c795e8a3f9ddf6698f3393f87bb8b7fc657fce6c
Bui Minh Duc <duc.bui@dinosys.vn>
2017-11-28 13:55:36 +0700

    Clean up

--- VBATicketCheckin/Models/Match.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Match.swift
-//  VBATicketCheckin
-//
-//  Created by Bui Minh Duc on 11/21/17.
-//  Copyright © 2017 Dinosys. All rights reserved.
-//
-
-import Foundation
--- VBATicketCheckin/ViewModels/LoginViewModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  LoginViewModel.swift
-//  VBATicketCheckin
-//
-//  Created by Bui Minh Duc on 11/22/17.
-//  Copyright © 2017 Dinosys. All rights reserved.
-//
-
-import Foundation
